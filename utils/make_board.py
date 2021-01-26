e5806f9ca13f3719fc8009c9953361da6d746183
TAG-Epic <tagepicuwu@gmail.com>
2021-01-26 18:37:09 +0000

    Feat: New ore type

--- utils/make_board.py
+++ utils/make_board.py
@@ -6,6 +6,6 @@ d = []
 for x in range(5):
     d.append([])
     for y in range(5):
-        d[x].append(choice([None, None, "tile:ore_fresh"]))
+        d[x].append(choice([None, None, "tile:ore_fresh", "tile:ore_used"]))
 
 print(dumps(d))
